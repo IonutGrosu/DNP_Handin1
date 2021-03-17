a58e5b832901c2c1b6b7c3c0ae5ce558e90572b4
IonutGrosu <ionutflorentin.grosu@gmail.com>
2021-03-17 19:13:31 +0100

    Fixed the spouse issue

--- DNP Handin1/Models/Person.cs
+++ DNP Handin1/Models/Person.cs
@@ -1,15 +1,21 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Models {
 public class Person {
     
     public int Id { get; set; }
+    [Required, MaxLength(32)]
     public string FirstName { get; set; }
+    [Required, MaxLength(32)]
     public string LastName { get; set; }
     public string HairColor { get; set; }
     public string EyeColor { get; set; }
+    [Range(0, Int32.MaxValue, ErrorMessage = "Please enter a valid age")]
     public int Age { get; set; }
+    [Range(0, Int32.MaxValue, ErrorMessage = "Please enter a valid weight")]
     public float Weight { get; set; }
+    [Range(0, Int32.MaxValue, ErrorMessage = "Please enter a valid height")]
     public int Height { get; set; }
     public string Sex { get; set; }
 }
