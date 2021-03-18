4f1225123e550f341a0c9b943d9dccbeccae006e
IonutGrosu <ionutflorentin.grosu@gmail.com>
2021-03-18 21:01:57 +0100

    Added functionality: *detailed child page *edit adult *edit child *remove child

--- DNP Handin1/Data/FileAdapter.cs
+++ DNP Handin1/Data/FileAdapter.cs
@@ -33,6 +33,22 @@ namespace DNP_Handin1.Data
             return null;
         }
 
+        public Family GetFamilyWithChild(int childId)
+        {
+            foreach (Family family in GetAllFamilies())
+            {
+                foreach (Child familyChild in family.Children)
+                {
+                    if (familyChild.Id == childId)
+                    {
+                        return family;
+                    }
+                }
+            }
+
+            return null;
+        }
+
         public List<Adult> GetAllAdults()
         {
             foreach (Adult fileContextAdult in FileContext.Adults)
@@ -64,6 +80,96 @@ namespace DNP_Handin1.Data
         public void RemoveAdult(int id)
         {
             FileContext.Adults.Remove(FileContext.Adults.First(t => t.Id==id));
+            FileContext.SaveChanges();
+        }
+
+        public void EditAdult(Adult editedAdult)
+        {
+            foreach (Adult fileContextAdult in FileContext.Adults)
+            {
+                if (fileContextAdult.Id == editedAdult.Id)
+                {
+                    fileContextAdult.JobTitle = editedAdult.JobTitle;
+                    fileContextAdult.FirstName = editedAdult.FirstName;
+                    fileContextAdult.LastName = editedAdult.LastName;
+                    fileContextAdult.HairColor = editedAdult.HairColor;
+                    fileContextAdult.EyeColor = editedAdult.EyeColor;
+                    fileContextAdult.Age = editedAdult.Age;
+                    fileContextAdult.Weight = editedAdult.Weight;
+                    fileContextAdult.Height = editedAdult.Height;
+                    fileContextAdult.Sex = editedAdult.Sex;
+                }
+            }
+
+            foreach (Family fileContextFamily in FileContext.Families)
+            {
+                foreach (Adult adult in fileContextFamily.Adults)
+                {
+                    if (adult.Id == editedAdult.Id)
+                    {
+                        adult.JobTitle = editedAdult.JobTitle;
+                        adult.FirstName = editedAdult.FirstName;
+                        adult.LastName = editedAdult.LastName;
+                        adult.HairColor = editedAdult.HairColor;
+                        adult.EyeColor = editedAdult.EyeColor;
+                        adult.Age = editedAdult.Age;
+                        adult.Weight = editedAdult.Weight;
+                        adult.Height = editedAdult.Height;
+                        adult.Sex = editedAdult.Sex;
+                    }
+                }
+            }
+            
+            FileContext.SaveChanges();
+        }
+
+        public List<Child> GetAllChildren()
+        {
+            List<Child> allChildren = new List<Child>();
+            foreach (Family family in GetAllFamilies())
+            {
+                foreach (Child familyChild in family.Children)
+                {
+                    allChildren.Add(familyChild);
+                }
+            }
+            return allChildren;
+        }
+
+        public Child GetChildById(int id)
+        {
+            return GetAllChildren().FirstOrDefault(child => child.Id == id);
+        }
+
+        public void RemoveChild(int id)
+        {
+            foreach (Family fileContextFamily in FileContext.Families)
+            {
+                fileContextFamily.Children.Remove(fileContextFamily.Children.FirstOrDefault(t => t.Id == id));
+            }
+            FileContext.SaveChanges();
+        }
+
+        public void EditChild(Child editedChild)
+        {
+            foreach (Family fileContextFamily in FileContext.Families)
+            {
+                foreach (Child child in fileContextFamily.Children)
+                {
+                    if (child.Id == editedChild.Id)
+                    {
+                        child.FirstName = editedChild.FirstName;
+                        child.LastName = editedChild.LastName;
+                        child.HairColor = editedChild.HairColor;
+                        child.EyeColor = editedChild.EyeColor;
+                        child.Age = editedChild.Age;
+                        child.Weight = editedChild.Weight;
+                        child.Height = editedChild.Height;
+                        child.Sex = editedChild.Sex;
+                    }
+                }
+            }
+            
             FileContext.SaveChanges();
         }
 
--- DNP Handin1/Data/IFileAdapter.cs
+++ DNP Handin1/Data/IFileAdapter.cs
@@ -8,10 +8,16 @@ namespace DNP_Handin1.Data
     {
         public List<Family> GetAllFamilies();
         public Family GetFamilyWithAdult(int adultId);
+        public Family GetFamilyWithChild(int childId);
         public List<Adult> GetAllAdults();
         public Adult GetAdultById(int id);
         public void AddAdult(Adult adult);
         public void RemoveAdult(int id);
+        public void EditAdult(Adult editedAdult);
+        public List<Child> GetAllChildren();
+        public Child GetChildById(int id);
+        public void RemoveChild(int id);
+        public void EditChild(Child editedChild);
         public List<Job> GetAllJobs();
     }
 }
